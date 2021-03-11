f4771c65438c1ded0032ce81162dd2eba4407997
Kevin Lyons <zinckev@hotmail.com>
2021-03-11 15:27:34 -0800

    got rating implemented from data.js along with review count

--- src/App.js
+++ src/App.js
@@ -1,55 +1,25 @@
-// import react from "react";
+import react from "react";
 import data from "./data";
+import Product from "./components/Product";
 
 function App() {
   return (
     <div className="gridContainer">
       <header className="row">
         <div>
-          <a className="brand" href="index.html">
+          <a className="brand" href="/">
             Da-Nile
           </a>
         </div>
         <div>
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
+          <a href="/cart">Cart</a>
+          <a href="/signIn">Sign In</a>
         </div>
       </header>
       <main>
         <div className="row center">
           {data.products.map((product) => (
-            <div key={product._id} className="card">
-              <a href={`/product/${product._id}`}>
-                <img
-                  className="medium"
-                  src={product.image}
-                  alt={product.name}
-                />
-              </a>
-              <div className="card-body">
-                <a href={`/product/${product._id}`}>
-                  <h2> {product.name}</h2>
-                </a>
-              </div>
-              <div className="rating">
-                <span>
-                  <i className="fas fa-star"></i>
-                </span>
-                <span>
-                  <i className="fas fa-star"></i>
-                </span>
-                <span>
-                  <i className="fas fa-star"></i>
-                </span>
-                <span>
-                  <i className="fas fa-star"></i>
-                </span>
-                <span>
-                  <i className="fas fa-star"></i>
-                </span>
-              </div>
-              <div class="price">${product.price}</div>
-            </div>
+            <Product key={product._id} product={product} />
           ))}
           ;
         </div>
