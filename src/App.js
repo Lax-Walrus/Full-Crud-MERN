e7c9f87b0259b4b30e18b11014258d5d9ebdc5a4
Kevin Lyons <zinckev@hotmail.com>
2021-03-11 17:07:36 -0800

    working on product page and conditional rendering of stock or unavailable

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import react from "react";
 import data from "./data";
 import Product from "./components/Product";
+import { BrowserRouter, Route } from "react-router-dom";
+import homeScreen from "./screens/homeScreen";
+import productScreen from "./screens/productScreen";
 
 function App() {
   return (
-    <div className="gridContainer">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">
-            Da-Nile
-          </a>
-        </div>
-        <div>
-          <a href="/cart">Cart</a>
-          <a href="/signIn">Sign In</a>
-        </div>
-      </header>
-      <main>
-        <div className="row center">
-          {data.products.map((product) => (
-            <Product key={product._id} product={product} />
-          ))}
-          ;
-        </div>
-      </main>
-      <footer className="row center">all rights reserved</footer>
-    </div>
+    <BrowserRouter>
+      <div className="gridContainer">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              Da-Nile
+            </a>
+          </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signIn">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Route path="/" component={homeScreen} exact></Route>
+          <Route path="/product/:id" component={productScreen}></Route>
+        </main>
+        <footer className="row center">all rights reserved</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
