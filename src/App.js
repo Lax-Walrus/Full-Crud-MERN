d2971d77716935fe2ae48ede46a1974caf843be3
Kevin Lyons <zinckev@hotmail.com>
2021-03-24 18:19:26 -0700

    added order screen and api

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import RegisterScreen from "./screens/registerScreen";
 import ShippingAddressScreen from "./screens/ShippingAddressScreen";
 import PaymentMethodScreen from "./screens/paymentMethodScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import OrderScreen from "./screens/OrderScreen";
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -61,6 +62,7 @@ function App() {
           <Route path="/shipping" component={ShippingAddressScreen}></Route>
           <Route path="/payment" component={PaymentMethodScreen}></Route>
           <Route path="/placeorder" component={PlaceOrderScreen}></Route>
+          <Route path="/order/:id" component={OrderScreen} exact></Route>
         </main>
         <footer className="row center">all rights reserved</footer>
       </div>
