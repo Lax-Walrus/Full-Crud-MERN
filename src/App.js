2a69834422950bb92da133f5ded22963e772b383
Kevin Lyons <zinckev@hotmail.com>
2021-04-09 12:02:46 -0700

    added private route for logging out on up-date screen fixing error with update profile

--- src/App.js
+++ src/App.js
@@ -10,7 +10,10 @@ import ShippingAddressScreen from "./screens/ShippingAddressScreen";
 import PaymentMethodScreen from "./screens/paymentMethodScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
+
 import OrderHistoryScreen from "./screens/OrderHistoryScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -45,6 +48,10 @@ function App() {
                 </Link>
                 <ul className="dropdown-content">
                   <li>
+                    <Link to="/profile"> My Profile </Link>
+                  </li>
+
+                  <li>
                     <Link to="/orderhistory"> Order History</Link>
                   </li>
                   <Link to="#signout" onClick={signoutHandler}>
@@ -54,6 +61,28 @@ function App() {
               </div>
             ) : (
               <Link to="/signIn">Sign In</Link>
+            )}
+
+            {userInfo && userInfo.isAdmin && (
+              <div className="dropdown">
+                {" "}
+                <i className="fa fa-caret-down"></i>
+                <Link to="#admin"> Admin </Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="/dashboard"> Dashboard </Link>
+                  </li>
+                  <li>
+                    <Link to="/productlist"> Products </Link>
+                  </li>
+                  <li>
+                    <Link to="/orderlist"> Orders </Link>
+                  </li>
+                  <li>
+                    <Link to="/userlist"> Dashboard </Link>
+                  </li>
+                </ul>
+              </div>
             )}
           </div>
         </header>
@@ -68,6 +97,10 @@ function App() {
           <Route path="/payment" component={PaymentMethodScreen}></Route>
           <Route path="/placeorder" component={PlaceOrderScreen}></Route>
           <Route path="/order/:id" component={OrderScreen} exact></Route>
+          <PrivateRoute
+            path="/profile"
+            component={ProfileScreen}
+          ></PrivateRoute>
         </main>
         <footer className="row center">all rights reserved</footer>
       </div>
--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -6,6 +6,13 @@ import {
   USER_SIGNIN_REQUEST,
   USER_SIGNIN_SIGNOUT,
   USER_SIGNIN_SUCCESS,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_FAIL,
+  USER_DETAILS_SUCCESS,
+  USER_UPDATED_PROFILE_REQUEST,
+  USER_UPDATED_PROFILE_SUCCESS,
+  USER_UPDATED_PROFILE_FAIL,
+  USER_UPDATED_PROFILE_RESET,
 } from "../constants/userConstants";
 
 export const userRegisterReducer = (state = {}, action) => {
@@ -38,6 +45,37 @@ export const userSigninReducer = (state = {}, action) => {
     case USER_SIGNIN_SIGNOUT:
       return {};
 
+    default:
+      return state;
+  }
+};
+
+export const userDetailsReducer = (state = { loading: true }, action) => {
+  switch (action.type) {
+    case USER_DETAILS_REQUEST:
+      return { loading: true };
+
+    case USER_DETAILS_SUCCESS:
+      return { loading: false, user: action.payload };
+
+    case USER_DETAILS_FAIL:
+      return { loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
+
+export const userUpdateProfileReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_UPDATED_PROFILE_REQUEST:
+      return { loading: true };
+    case USER_UPDATED_PROFILE_SUCCESS:
+      return { loading: false, success: true };
+    case USER_UPDATED_PROFILE_FAIL:
+      return { loading: false, error: action.payload };
+    case USER_UPDATED_PROFILE_RESET:
+      return state;
     default:
       return state;
   }
