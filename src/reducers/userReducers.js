d901af6116a6a9b6773046aba71828d6be03b5ac
Kevin Lyons <zinckev@gmail.com>
2021-05-14 12:42:04 -0700

    added seller carousel for top rated sellers

--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -25,6 +25,9 @@ import {
   USER_UPDATE_FAIL,
   USER_UPDATE_RESET,
   USER_DETAILS_RESET,
+  USER_TOPSELLERS_LIST_FAIL,
+  USER_TOPSELLERS_LIST_SUCCESS,
+  USER_TOPSELLERS_LIST_REQUEST,
 } from "../constants/userConstants";
 
 export const userRegisterReducer = (state = {}, action) => {
@@ -141,6 +144,22 @@ export const userUpdateReducer = (state = {}, action) => {
       return { loading: false, error: action.payload };
     case USER_UPDATE_RESET:
       return state;
+    default:
+      return state;
+  }
+};
+
+export const userTopSellerListReducer = (state = { loading: true }, action) => {
+  switch (action.type) {
+    case USER_TOPSELLERS_LIST_REQUEST:
+      return { loading: true };
+
+    case USER_TOPSELLERS_LIST_SUCCESS:
+      return { loading: false, users: action.payload };
+
+    case USER_TOPSELLERS_LIST_FAIL:
+      return { loading: false, error: action.payload };
+
     default:
       return state;
   }
